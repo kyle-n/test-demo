fb99ac30b54347be72df1fc8b6272056491f1592
Kyle Nazario <knazario@bitovi.com>
2023-07-27 14:40:12 -0400

    Inlines component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { JokeTellerService } from './joke-teller.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <header>
+      <h1>Jokebot 9000</h1>
+    </header>
+    <main>
+      <div *ngIf="jokeCount > 0">Joke #{{ jokeCount }}:
+        <span id="good-joke">{{ joke }}</span>
+      </div>
+      <button (click)="newJoke()">Tell me a new joke</button>
+    </main>
+  `
 })
 export class AppComponent {
   jokeCount = 0;
