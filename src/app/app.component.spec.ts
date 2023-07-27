dc5e05de3f17e89d50ab478a426c17aace097aae
Kyle Nazario <knazario@bitovi.com>
2023-07-27 14:38:01 -0400

    Unit tests component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JokeTellerService } from './joke-teller.service';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [JokeTellerService]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  function clickJokeButton(): void {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement as HTMLButtonElement;
+    button.dispatchEvent(new Event('click'));
+  }
 
-  it(`should have as title 'test-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-demo');
-  });
+  function displayedJoke(): string | null {
+    const jokeContainer = fixture.debugElement.query(By.css('#good-joke'))?.nativeElement as HTMLSpanElement | null;
+    if (jokeContainer) {
+      return jokeContainer.innerText.trim()
+    } else {
+      return null;
+    }
+  }
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-demo app is running!');
-  });
+  it('should tell a joke when the user clicks the button', () => {
+    expect(displayedJoke()).toBeNull()
+    clickJokeButton()
+    fixture.detectChanges()
+    expect(displayedJoke()).not.toBeNull();
+  })
 });
