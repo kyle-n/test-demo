917f1b0ae13c5cfe3939605b9f92e40b0ae10560
Kyle Nazario <knazario@bitovi.com>
2023-07-27 13:36:47 -0400

    Great jokes

--- src/app/joke-teller.service.ts
+++ src/app/joke-teller.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class JokeTellerService {
+  // Courtesy https://www.scarymommy.com/popsicle-jokes
+  private jokes = [
+    `How did the telephone propose to his girlfriend?
+     He gave her a ring.`,
+    `What crew mans a haunted ship?
+     A skeleton crew.`,
+    `Where do cows go for entertainment?
+     Moo-vies.`,
+  ]
 
-  constructor() { }
+  private jokeIndex = 0;
+
+  tellJoke(): string {
+    const jokeToTell = this.jokes[this.jokeIndex];
+    this.jokeIndex = (this.jokeIndex + 1) % this.jokes.length;
+    return jokeToTell;
+  }
 }
